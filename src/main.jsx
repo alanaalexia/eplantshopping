1c76a0dea81ff64c7021b5f144fe569b0a5d9cbd
alanaalexia <alanaalexia.h@gmail.com>
2025-09-26 20:59:36 -0300

    Configurações para deploy no github

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import store from './store'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { HashRouter } from "react-router-dom"; // <<<
+import App from "./App.jsx";
+import store from "./store.js";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-<React.StrictMode>
-<Provider store={store}>
-<BrowserRouter>
-<App />
-</BrowserRouter>
-</Provider>
-</React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <HashRouter>     {/* <<< */}
+        <App />
+      </HashRouter>
+    </Provider>
+  </React.StrictMode>
 );
